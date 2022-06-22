718936b225cb68dd14a0bd44ac8ae833f41c4ae5
andrey-rsn <korowai98.ag@gmail.com>
2022-06-22 22:04:27 +0300

    Create and set up Catalog API

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,14 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+        }
+        public IMongoCollection<Product> Products => throw new NotImplementedException();
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Entities/Product.cs
@@ -0,0 +1,31 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Catalog.API.Entities
+{
+    public class Product
+    {
+        [BsonId]
+        [BsonRepresentation(MongoDB.Bson.BsonType.ObjectId)]
+        public string Id { get; set; }
+
+        [BsonElement("Name")]
+        public string Name { get; set; }
+
+        [BsonElement("Category")]
+        public string Category { get; set; }
+
+        [BsonElement("Summary")]
+        public string Summary { get; set; }
+
+        [BsonElement("Description")]
+        public string Description { get; set; }
+
+        [BsonElement("ImageFile")]
+        public string ImageFile { get; set; }
+
+        [BsonElement("Price")]
+        public decimal Price { get; set; }
+
+
+    }
+}
