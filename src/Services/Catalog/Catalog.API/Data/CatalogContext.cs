e769b3fb8994531e5c88e8e8ea8c289ee6937e34
andrey-rsn <korowai98.ag@gmail.com>
2022-06-22 22:26:06 +0300

    set up ProductDb context

--- src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -8,7 +8,11 @@ namespace Catalog.API.Data
         public CatalogContext(IConfiguration configuration)
         {
             var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
+
+            Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+            CatalogContextSeed.SeedData(Products);
         }
-        public IMongoCollection<Product> Products => throw new NotImplementedException();
+        public IMongoCollection<Product> Products { get; }
     }
 }
