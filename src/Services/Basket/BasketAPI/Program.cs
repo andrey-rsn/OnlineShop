4a7415598a0f615539971988ab8359a7d824ec0a
andrey-rsn <korowai98.ag@gmail.com>
2022-07-05 21:18:40 +0300

    configure docker-compose for discountgrpc

--- src/Services/Basket/BasketAPI/Program.cs
+++ src/Services/Basket/BasketAPI/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddStackExchangeRedisCache(options =>
     options.Configuration = Configuration.GetValue<string>("CacheSettings:ConnectionString");
 });
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
-builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(o=>o.Address= new Uri(Configuration["GrpcSettings:DiscountUrl"]));
+builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(o=>o.Address= new Uri(Configuration.GetValue<string>("GrpcSettings:DiscountUrl")));
 builder.Services.AddScoped<DiscountGrpcService>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
