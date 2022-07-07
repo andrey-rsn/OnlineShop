d60b7aa6a07e52041dce490e1d3caac86ead1fcf
andrey-rsn <korowai98.ag@gmail.com>
2022-07-07 13:22:03 +0300

    Create Queries

---
+++ src/Services/Ordering/OrderingApplication/Features/Orders/Queries/GetOrdersList/GetOrdersListQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+
+namespace OrderingApplication.Features.Orders.Queries.GetOrdersList
+{
+    public class GetOrdersListQuery : IRequest<List<OrdersVm>>
+    {
+        public string UserName { get; set; }
+
+        public GetOrdersListQuery(string userName)
+        {
+            UserName = userName;
+        }
+    }
+}
--- src/Services/Ordering/OrderingApplication/Mappings/MappingProfile.cs
+++ src/Services/Ordering/OrderingApplication/Mappings/MappingProfile.cs
@@ -1,9 +1,14 @@
 ﻿using AutoMapper;
+using OrderingApplication.Features.Orders.Queries.GetOrdersList;
+using OrderingDomain.Entities;
 
 namespace OrderingApplication.Mappings
 {
     public class MappingProfile : Profile
     {
-
+        public MappingProfile()
+        {
+            CreateMap<Order, OrdersVm>().ReverseMap();
+        }
     }
 }
