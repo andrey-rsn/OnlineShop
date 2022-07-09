70a67165a01bf8b81875db7b9603b8eda464b9c5
andrey-rsn <korowai98.ag@gmail.com>
2022-07-09 14:09:35 +0300

    Configure Order Infrastructure DI

--- src/Services/Ordering/OrderingAPI/Program.cs
+++ src/Services/Ordering/OrderingAPI/Program.cs
@@ -1,9 +1,11 @@
 using OrderingApplication;
+using OrderingInfrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
-
+ConfigurationManager configuration = builder.Configuration;
 // Add services to the container.
 builder.Services.AddApplicationServices();
+builder.Services.AddinfrastructureServices(configuration);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
---
+++ src/Services/Ordering/OrderingInfrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OrderingApplication.Contracts.Infrastructure;
+using OrderingApplication.Contracts.Persistance;
+using OrderingApplication.Models;
+using OrderingInfrastructure.Persistance;
+using OrderingInfrastructure.Repositories;
+using OrderingInfrastructure.Services.Mail;
+
+namespace OrderingInfrastructure
+{
+    public static class InfrastructureServiceRegistration
+    {
+        public static IServiceCollection AddinfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<OrderContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("OrderingConnectionString")));
+
+            services.AddScoped(typeof(IAsyncRepository<>), typeof(RepositoryBase<>));
+            services.AddScoped<IOrderRepository, OrderRepository>();
+
+            services.Configure<EmailSettings>(c => configuration.GetSection("EmailSettings"));
+            services.AddTransient<IEmailService, EmailService>();
+
+            return services;
+        }
+    }
+}
