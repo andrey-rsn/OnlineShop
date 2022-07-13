83ee0c12cf803cec11b8999aed2fb46555da0185
andrey-rsn <korowai98.ag@gmail.com>
2022-07-13 19:14:31 +0300

    Connect BasketApi to rabbitmq

--- src/Services/Basket/BasketAPI/Program.cs
+++ src/Services/Basket/BasketAPI/Program.cs
@@ -1,14 +1,22 @@
 using BasketAPI.GrpcServices;
 using BasketAPI.Repositories;
 using DiscountGrpc.Protos;
+using MassTransit;
 
 var builder = WebApplication.CreateBuilder(args);
 ConfigurationManager Configuration = builder.Configuration;
 // Add services to the container.
+builder.Services.AddAutoMapper(typeof(Program));
 builder.Services.AddStackExchangeRedisCache(options =>
 {
     options.Configuration = Configuration.GetValue<string>("CacheSettings:ConnectionString");
 });
+builder.Services.AddMassTransit(config => {
+    config.UsingRabbitMq((ctx, cfg) => {
+        cfg.Host(Configuration["EventBusSettings:HostAddress"]);
+    });
+});
+//builder.Services.AddMassTransitHostedService();
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
 builder.Services.AddGrpcClient<DiscountProtoService.DiscountProtoServiceClient>(o=>o.Address= new Uri(Configuration.GetValue<string>("GrpcSettings:DiscountUrl")));
 builder.Services.AddScoped<DiscountGrpcService>();
