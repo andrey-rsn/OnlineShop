ee640a7d69a81807dfc808980729612fc47665c9
andrey-rsn <korowai98.ag@gmail.com>
2022-07-16 16:22:33 +0300

    Created main page components

---
+++ src/WebClient/online-shop/src/components/App/App.js
@@ -0,0 +1,22 @@
+import { Navbar } from '../Navbar/Navbar';
+import '../../styles/App.css';
+import {ProductList} from '../ProductList/ProductList'
+import Pagination from '@mui/material/Pagination';
+import Grid from '@mui/material/Grid';
+
+function App() {
+  return (
+    <>
+        <Navbar/>
+      <div class="container" style={{marginBottom:'60px'}}>
+        <ProductList/> 
+      </div>
+        <Grid container justifyContent="center" className='fixed-bottom mb-2'>
+           <Pagination count={10} color="primary" size="large"/>
+        </Grid>
+    </>
+      
+  );
+}
+
+export default App;
---
+++ src/WebClient/online-shop/src/components/ProductItem/ProductItem.jsx
@@ -0,0 +1,37 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import image from '../../logo.svg';
+import Button from '@mui/material/Button';
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+  minWidth:'300px',
+  minHeight: '200px'
+}));
+
+export const ProductItem = ()=> {
+  return (
+        <Grid item>
+          <Item style={{textAlign:'left'}}>
+            <img src={image} alt="" />
+            <hr />
+            <div className='mb-5'>
+                <div style={{display:'flex',flexDirection:'row',justifyContent:'space-between',fontSize:'16px', fontWeight:'600',marginRight:'5px'}}>
+                    <p style={{}}>Цена:</p>
+                    <p>1500p</p>
+                </div>
+                
+                <h1 style={{fontSize:'12px'}}>Продукт такой то такой то хороший дешевый</h1>
+            </div>
+            <Button variant="contained">В корзину</Button>
+            
+          </Item>
+        </Grid>
+  );
+}
