3fd2aa4cf094ab6d8c28c4e30e563d4b6db9f1c2
andrey-rsn <korowai98.ag@gmail.com>
2022-07-18 20:34:17 +0300

    Configure Gateway settings and cache settings

--- src/ApiGateways/OcelotApiGetaway/Program.cs
+++ src/ApiGateways/OcelotApiGetaway/Program.cs
@@ -1,9 +1,13 @@
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Ocelot.Cache.CacheManager;
 
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddOcelot();
+builder.Host.ConfigureAppConfiguration((hostingContext, config) =>
+{
+    config.AddJsonFile($"ocelot.{hostingContext.HostingEnvironment.EnvironmentName}.json",true,true);
+});
+builder.Services.AddOcelot().AddCacheManager(options=>options.WithDictionaryHandle());
 
 var app = builder.Build();
 
